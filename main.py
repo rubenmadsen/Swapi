13c83dbdedd98c782cf3ae090673c725465ec200
Ruben Madsen <ruben@rubenmadsen.com>
2024-09-26 23:25:21 +0200

    slask

--- main.py
+++ main.py
@@ -2,10 +2,42 @@ import os
 
 import requests
 import psycopg2
+import json
 
 url = "https://swapi.dev/api/"
-people_url = f"{url}people/"
 db = None
+films = set()
+planets = set()
+spaceships = set()
+vehicles = set()
+people = set()
+species = set()
+
+films.add(f"{url}films/1/")
+films.add(f"{url}films/2/")
+films.add(f"{url}films/3/")
+films.add(f"{url}films/4/")
+films.add(f"{url}films/5/")
+films.add(f"{url}films/6/")
+
+def parse_film(film):
+    parent = ''
+    keys = set()
+    json_data = {}
+    if isinstance(parent, str):
+        for k, v in film.items():
+            #full_key = k #f"{parent}.{k}" if parent else k
+            keys.add(k)
+            json_data[k] = v
+        if isinstance(parent, list):
+        print(f"Dolk")
+    print(f"Data: {json_data}")
+
+def pull_data():
+    for film in films:
+        response = requests.get(film)
+        print(response.status_code)
+        parse_film(json.loads(response.text))
 def open_db(password):
     global db
     try:
@@ -27,12 +59,23 @@ def open_db(password):
         print(f"An error occurred: {e}")
 
 def run():
-    response = requests.get(f"{people_url}{8}")
-    print(response.json())
+    pass
+    #response = requests.get(f"{people_url}{8}")
+    #data = json.loads(response.text)
+    #keys = set()
+    #parent = ''
+    #for k, v in data.items():
+        #full_key = f"{parent}.{k}" if parent else k
+        #keys.add(full_key)
+        #print("Key: " + full_key)
+
+
+    #print(keys)
 
 
 
 if __name__ == "__main__":
     pw = os.environ.get('POSTGRES_PASSWORD')
     open_db(pw)
+    pull_data()
     run()
